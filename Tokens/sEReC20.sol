eb1664d1965370823ce1f9615202c2c04d070570
Serec Thunderson <83026393+SerecThunderson@users.noreply.github.com>
2023-07-07 19:35:12 -0800

    Update sEReC20.sol
    immutable - del 256

--- Tokens/sEReC20.sol
+++ Tokens/sEReC20.sol
@@ -1,14 +1,14 @@
 pragma solidity 0.8.18;
 contract sEReC20 {
-    mapping(address => uint256) private _balances;
-    mapping(address => mapping(address => uint256)) private _allowances;
-    uint256 private _totalSupply;
-    string private _name;
-    string private _symbol;
-    uint8 private _decimals;
+    mapping(address => uint) private _balances;
+    mapping(address => mapping(address => uint)) private _allowances;
+    uint private _totalSupply;
+    string private immutable _name;
+    string private immutable _symbol;
+    uint8 private immutable _decimals;
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
 
     constructor(string memory name_, string memory symbol_, uint8 decimals_, uint supply_) {
         _name = name_;
@@ -21,40 +21,40 @@ contract sEReC20 {
     function name() external view returns (string memory) {return _name;}
     function symbol() external view returns (string memory) {return _symbol;}
     function decimals() external view returns (uint8) {return _decimals;}
-    function totalSupply() external view returns (uint256) {return _totalSupply;}
-    function balanceOf(address account) external view returns (uint256) {return _balances[account];}
-    function allowance(address owner, address spender) external view returns (uint256) {return _allowances[owner][spender];}
+    function totalSupply() external view returns (uint) {return _totalSupply;}
+    function balanceOf(address account) external view returns (uint) {return _balances[account];}
+    function allowance(address owner, address spender) external view returns (uint) {return _allowances[owner][spender];}
 
-    function transfer(address to, uint256 amount) public returns (bool) {
+    function transfer(address to, uint amount) public returns (bool) {
         _transfer(msg.sender, to, amount);
         return true;
     }
 
-    function approve(address spender, uint256 amount) public returns (bool) {
+    function approve(address spender, uint amount) public returns (bool) {
         _approve(msg.sender, spender, amount);
         return true;
     }
 
-    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
+    function transferFrom(address from, address to, uint amount) public returns (bool) {
         _spendAllowance(from, msg.sender, amount);
         _transfer(from, to, amount);
         return true;
     }
 
-    function _transfer(address from, address to, uint256 amount) internal {
+    function _transfer(address from, address to, uint amount) internal {
         require(_balances[from] >= amount, "ERC20: transfer amount exceeds balance");
         _balances[from] -= amount;
         _balances[to] += amount;
         emit Transfer(from, to, amount);
     }
 
-    function _approve(address owner, address spender, uint256 amount) internal {
+    function _approve(address owner, address spender, uint amount) internal {
         _allowances[owner][spender] = amount;
         emit Approval(owner, spender, amount);
     }
 
-    function _spendAllowance(address owner, address spender, uint256 amount) internal {
-        uint256 currentAllowance = allowance(owner, spender);
+    function _spendAllowance(address owner, address spender, uint amount) internal {
+        uint currentAllowance = allowance(owner, spender);
         require(currentAllowance >= amount, "ERC20: insufficient allowance");
         _approve(owner, spender, currentAllowance - amount);
     }
