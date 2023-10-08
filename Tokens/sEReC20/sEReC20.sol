f47ec4d7d54210f7a13e2359802ef35a1d03f899
Serec Thunderson <83026393+SerecThunderson@users.noreply.github.com>
2023-10-08 12:51:02 -0500

    Add files via upload

---
+++ Tokens/sEReC20/sEReC20.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract sEReC20 {
+
+    string public name;
+    string public symbol;
+    uint public decimals;
+    uint public totalSupply;
+
+    mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+
+    constructor(string memory name_, string memory symbol_, uint decimals_, uint supply_) {
+        name = name_; symbol = symbol_; decimals = decimals_;
+        totalSupply = supply_ * 10 ** decimals_;
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function approve(address owner, address spender, uint amount) public returns (bool) {
+        allowance[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+        return true;
+    }
+
+    function transfer(address to, uint amount) public returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint amount) public returns (bool) {
+        _spendAllowance(from, msg.sender, amount);
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    function _transfer(address from, address to, uint amount) internal {
+        require(balanceOf[from] >= amount, "ERC20: transfer amount exceeds balance");
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
+    function _spendAllowance(address owner, address spender, uint amount) internal {
+        require(allowance[owner][spender] >= amount, "ERC20: insufficient allowance");
+        approve(owner, spender, allowance[owner][spender] - amount);
+    }
+
+}
---
+++ Utils/Staking/StakingCore.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract StakingContract is sEReC20("Staked Ethereum", "sETH", 18, 0) {
+
+    uint256 public totalStaked;
+
+    function stake() public payable {
+        require(msg.value > 0, "Cannot stake 0 ETH");
+        uint256 stakingTokens = (totalStaked == 0) ? msg.value : (msg.value * totalSupply()) / totalStaked;
+        totalStaked += msg.value;
+        _mint(msg.sender, stakingTokens);
+    }
+
+    function unstake(uint256 amount) public {
+        require(amount > 0 && amount <= balanceOf(msg.sender), "Invalid unstake amount");
+        uint256 ethAmount = (amount * totalStaked) / totalSupply();
+        require(ethAmount > 0, "Not enough ETH to unstake");
+        _burn(msg.sender, amount);
+        totalStaked -= ethAmount;
+        payable(msg.sender).transfer(ethAmount);
+    }
+
+}
