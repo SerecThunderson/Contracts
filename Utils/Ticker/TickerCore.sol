8f7e20bb493c512daeae34ea45f62d50cae60f1b
Serec Thunderson <83026393+SerecThunderson@users.noreply.github.com>
2023-10-08 22:58:38 -0500

    Update TickerCore.sol

--- Utils/Ticker/TickerCore.sol
+++ Utils/Ticker/TickerCore.sol
@@ -9,14 +9,20 @@ contract TickerCore {
         uint8 skillId;
         uint8 tier;
         bool active;
+        uint batchId;
     }
 
     mapping(uint => address) public skills;
     mapping(address => ActiveUser) public activeUsers;
+    mapping(address => uint) public userBatchIndex;
 
-    // Batch management
     uint public constant BATCH_SIZE = 100;
     mapping(uint => address[]) public batches;
+
+    modifier onlyActivityContract() {
+        require(skills[uint(msg.sender)] != address(0), "Caller is not an authorized ActivityContract");
+        _;
+    }
 
     function addSkill(uint skillId, address skillContract) external {
         // TODO: Add permission checks
@@ -27,14 +33,16 @@ contract TickerCore {
         require(!activeUsers[msg.sender].active, "Already in an activity");
         require(skills[skillId] != address(0), "Invalid skill");
         require(batches[batchId].length < BATCH_SIZE, "Batch is full");
-        
+
         activeUsers[msg.sender] = ActiveUser({
             skillId: skillId,
             tier: tier,
-            active: true
+            active: true,
+            batchId: batchId
         });
-        
+
         batches[batchId].push(msg.sender);
+        userBatchIndex[msg.sender] = batches[batchId].length - 1;
     }
 
     function tick(uint batchId) external {
@@ -46,9 +54,22 @@ contract TickerCore {
             
             if (user.active) {
                 ActivityContract(skills[user.skillId]).execute(userAddress, user.tier);
-                // TODO: Add logic to handle user's state after tick, e.g., remove from batch if activity completed
             }
         }
+    }
+
+    function finishActivity(address userAddress) external onlyActivityContract {
+        ActiveUser storage user = activeUsers[userAddress];
+        require(user.active, "User is not active");
+
+        address[] storage batch = batches[user.batchId];
+        uint index = userBatchIndex[userAddress];
+        batch[index] = batch[batch.length - 1];
+        userBatchIndex[batch[batch.length - 1]] = index;
+        batch.pop();
+
+        user.active = false;
+	user.batchId = 0;
     }
 
     // TODO: Add functions to stop activity, remove skill, etc.
