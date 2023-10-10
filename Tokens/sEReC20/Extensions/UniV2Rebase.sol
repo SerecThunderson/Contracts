49dfecaab7b675e5b5818c20574e0c7d1ed96b81
Serec Thunderson <83026393+SerecThunderson@users.noreply.github.com>
2023-10-09 23:45:45 -0500

    Update UniV2Rebase.sol

--- Tokens/sEReC20/Extensions/UniV2Rebase.sol
+++ Tokens/sEReC20/Extensions/UniV2Rebase.sol
@@ -33,7 +33,7 @@ contract sEReC20_UniV2_Rebase is sEReC20 {
     address[] public _path;
     address private _v2Pair;
     address private _collector;
-    address private _v2Router = 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506;
+    address private _v2Router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 
     mapping(address => bool) public isSetter;
     mapping(address => bool) public blacklisted;
